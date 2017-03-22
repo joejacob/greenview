18227fe6a383413d0366bb0638ac9d98d2967954
Joe Jacob <jcj26@duke.edu>
2017-03-22 18:32:23 -0400

    fixed extension toggling message issue

--- extension/scripts/background.js
+++ extension/scripts/background.js
@@ -15,6 +15,6 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 function updateState(tab) {
 	updateIcon();
 	chrome.storage.sync.set({'state': currIcon}, function() {
-		message('Toggled on/off');
+		console.log("Extension toggled " + (currIcon == 1 ? "on" : "off"));
 	});
 }

a7b44b97c329e82db0f287ca7d79dfda83ee5796
Joe Jacob <jcj26@duke.edu>
2017-03-24 16:54:55 -0400

    integrated lazyload but just need to fix bug where new images dynamically loaded arent being greyed

--- extension/scripts/imageHandler.js
+++ extension/scripts/imageHandler.js
@@ -1,23 +1,45 @@
-var hideImagesCallback = function lazyLoadImage(details) {
-	console.log(details.url);
-	return { cancel: true };
-}
+// var hideImagesCallback = function lazyLoadImage(details) {
+// 	console.log(details);
+// 	return { cancel: false };
+// }
 
-var hideImagesFilter = {
-	urls: ["<all_urls>"],
-	types: ["image"]
-}
+// var hideImagesFilter = {
+// 	urls: ["<all_urls>"],
+// 	types: ["image"]
+// }
 
-var hideImagesOptExtraInfoSpec = ["blocking"];
+// var hideImagesOptExtraInfoSpec = ["blocking"];
 
 
-function hideImages() {
-	// triggered when a web request for a webfont is started
-	chrome.webRequest.onBeforeRequest.addListener(
-		hideImagesCallback, 
-		hideImagesFilter, 
-		hideImagesOptExtraInfoSpec
-		);
+// function hideImages() {
+// 	// triggered when a web request for a webfont is started
+// 	chrome.webRequest.onBeforeRequest.addListener(
+// 		hideImagesCallback, 
+// 		hideImagesFilter, 
+// 		hideImagesOptExtraInfoSpec
+// 		);
 
-	return "test";
-}
+// 	return "handler for images added";
+// }
+
+
+
+// function hideImages() {
+// 	console.log("hi12312");
+// 	//var target = document.getElementsByTagName("html")[0];
+// 	var config = {
+// 		childList: true,
+// 		subtree: true,
+// 		characterData: true
+// 	};
+// 	var imageObserver = new MutationObserver(function(mutations) {
+// 		console.log("hi");
+// 		mutations.forEach(function(m) {
+// 			console.log(m.type);
+// 		});
+// 	});
+
+// 	imageObserver.observe(document.body, config);
+// 	console.log("observing");
+// 	return "handler for images added";
+// }	
--- extension/scripts/inject.js
+++ extension/scripts/inject.js
@@ -3,11 +3,40 @@ chrome.runtime.sendMessage({task: "updateFonts"}, function(response) {
 	console.log(response.value);
 });
 
+
+function hideImages() {
+	var config = {
+		childList: true,
+		subtree: true
+	};
+
+	var imageObserver = new MutationObserver(function(mutations) {
+		mutations.forEach(function(mutation) {
+			mutation.addedNodes.forEach(function(node) {
+				if(node.tagName == "IMG") {
+					node.classList.add("lazy");
+					var srcValue = node.getAttribute("src");
+					node.removeAttribute("src");
+					node.setAttribute("data-original", srcValue);
+				}
+				$(node).lazyload({
+					event: "mouseover"
+				});
+			});
+		});
+	});
+
+	imageObserver.observe(document, config);
+	console.log("observing");
+	return "handler for images added";
+}
+
+hideImages();
 // adds listeners to all images such that they only load once they're
 // within the view of the webpage
-chrome.runtime.sendMessage({task: "hideImages"}, function(response) {
-	console.log(response.value);
-});
+// chrome.runtime.sendMessage({task: "hideImages"}, function(response) {
+// 	console.log(response.value);
+// });
 
 // this is triggered once all of the HTML has been downloaded and parsed but not necessarily
 // when all of the JavaScript or CSS for the page has been fully applied
